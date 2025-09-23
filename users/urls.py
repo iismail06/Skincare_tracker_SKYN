646c3eabadc3533f7ca921ebd9b25851d7993e6f
idil <idili@hotmail.co.uk>
2025-09-23 11:42:15 +0100

    feat: initialize user URL patterns in urls.py and create Django admin

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'users'
+urlpatterns = [
+    # Add your user-related URL patterns here
+    # Example: path('', views.index, name='index'),
+]
