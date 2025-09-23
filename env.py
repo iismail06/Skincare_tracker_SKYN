1e51d4e134bfe0eae76615da33ecfad993b3afef
idil <idili@hotmail.co.uk>
2025-09-23 10:27:42 +0100

    Add initial Django project structure with configuration files

---
+++ env.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ.setdefault('SECRET_KEY', 'django-insecure-6t5u+v-!o#a8ozum4hdehs3xkve)i*$j-d6=e!+7-&&r(@s&)s')
+os.environ.setdefault('DEBUG', 'True')
+
+# os.environ.setdefault(
+#     "DATABASE_URL", "")
+# os.environ.setdefault(
+#     "CLOUDINARY_URL", "")
