c9aebe79359da8f38c6f948316c7e47cf0742cde
idil <idili@hotmail.co.uk>
2025-09-23 10:47:13 +0100

    feat: add user profile model and update URL configuration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(null=True, blank=True)
+    skin_type = models.CharField(max_length=50, choices=[
+        ('Oily', 'Oily'),
+        ('Dry', 'Dry'),
+        ('Combination', 'Combination'),
+        ('Sensitive', 'Sensitive'),
+    ])
+    skin_concerns = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
