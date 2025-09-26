3ee7eeedd954b616b6ea250e076dd1593371c6a9
idil <idili@hotmail.co.uk>
2025-09-26 13:44:46 +0100

    feat: Add checklist route to routines URL configuration

--- routines/urls.py
+++ routines/urls.py
@@ -6,4 +6,5 @@ app_name = 'routines'
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('add/', views.add_routine, name='add_routine'),
+     path('checklist/', views.routine_checklist_view, name='checklist'),
 ]
