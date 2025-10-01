6d540160413726c4b5e3d992e82ad385d7ed0a76
idil <idili@hotmail.co.uk>
2025-10-01 11:41:15 +0100

    feat: Add products app with initial configuration and structure

--- config/settings.py
+++ config/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'routines',
+    'products',
 ]
 
 SITE_ID = 1
---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'products'
+
+urlpatterns = [
+    # Product URLs will be added here as we build the views
+]
